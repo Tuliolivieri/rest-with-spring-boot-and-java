f67f112fe6538c8973833de47ae39b9b9cd26146
Tulio Olivieri <tuliolivieri@gmail.com>
2022-09-05 13:27:22 -0300

    First steps in Java with Spring Boot 3

---
+++ secao05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/tuliolivieri/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.tuliolivieri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
