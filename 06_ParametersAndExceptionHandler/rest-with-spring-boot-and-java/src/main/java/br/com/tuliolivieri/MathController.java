b8ef619dbabbaa8c1b1566e4a74bcdb6ad5dc9a8
Tulio Olivieri <tuliolivieri@gmail.com>
2022-09-07 21:26:30 -0300

    create math Controller and ExceptionHandler

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/br/com/tuliolivieri/MathController.java
@@ -0,0 +1,45 @@
+package br.com.tuliolivieri;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tuliolivieri.exceptions.UnsupportedMathOperationException;
+
+@RestController
+public class MathController {
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+	) throws Exception {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set numeric values");
+		}
+		
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		if(isNumeric(number)) return Double.parseDouble(strNumber);
+		
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
