8307acfcb0cbf87ac28479756408eed69851fe5f
Tulio Olivieri <tuliolivieri@gmail.com>
2023-02-26 21:17:34 -0300

    feat: testing CORS

--- 16_CORS/rest-with-spring-boot-and-java/src/test/java/br/com/tuliolivieri/configs/TestConfigs.java
+++ 16_CORS/rest-with-spring-boot-and-java/src/test/java/br/com/tuliolivieri/configs/TestConfigs.java
@@ -2,4 +2,14 @@ package br.com.tuliolivieri.configs;
 
 public class TestConfigs {
 	public static final int SERVER_PORT = 8888;
+
+	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_XML = "application/xml";
+	public static final String CONTENT_TYPE_YML = "application/x-yaml";
+	
+	public static final String ORIGIN_TULIOLIVIERI = "https://tuliolivieri.github.io";
+	public static final String WRONG_ORIGIN = "https://tulioolivieri.github.io";
 }
